299a7e257701f1a5419b37243f2da5f827b32e2e
Cin💜 Lo <cinlos3382@gmail.com>
2023-05-07 15:25:35 -0300

    Parámetros opcionales y nullish-coalescing

---
+++ src/05-optionals.ts
@@ -0,0 +1,21 @@
+export const createProduct = (
+  id: string | number,
+  isNew?: boolean,
+  stock?: number,
+) => {
+  return {
+    id,
+    stock: stock ?? 10,
+    isNew: isNew ?? true
+  }
+
+}
+
+const product1 = createProduct(1,true,12);
+console.log(product1);
+
+const product2 = createProduct(1);
+console.log(product2);
+
+const product3 = createProduct(1, false, 0);
+console.log(product3);
