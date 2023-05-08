e6edd1b454336cdccfd0af82f0778f61a2ecc63e
Cin💜 Lo <cinlos3382@gmail.com>
2023-05-08 14:01:55 -0300

    ejemplo de crud en el proyecto app

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,15 +1,27 @@
-import { addProduct } from "./products/product.service";
+import {faker} from '@faker-js/faker';
+import { addProduct, products } from "./products/product.service";
 
-addProduct({
-  id: '1',
-  title: 'p1',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  stock: 5,
-  category: {
-    id: '12',
-    name: 'c1',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  }
-});
+for(let index= 0; index<= 50; index++) {
+  addProduct({
+    id: faker.datatype.uuid(),
+    description: faker.commerce.productDescription(),
+    image: faker.image.imageUrl(),
+    color: faker.color.human(),
+    price: parseInt(faker.commerce.price(), 10),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.arrayElements(),
+    title: faker.commerce.productName(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    stock: faker.datatype.number({min:10, max:100}),
+    size: faker.helpers.arrayElement(['S','M', 'L', 'XL']),
+    category: {
+      id: faker.datatype.uuid(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    }
+  });
+}
+
+console.log(products);
